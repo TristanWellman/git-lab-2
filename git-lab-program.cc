2682d0d43fae2aef296d19bb159bd5e8966ba602
TristanWellman <wellmanboy06@gmail.com>
2024-09-05 16:22:27 -0400

    c++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,15 @@
+/**
+ *	File: git-lab-program.cc
+ *	Author: Tristan Wellman
+ *	Date: 9/5/24
+ *	Description: C++ program
+ * */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello Git!!" << endl;
+	return 0;
+}
